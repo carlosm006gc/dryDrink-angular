3453d76aacc88dd4d2c8d79c8b21f7e6f95ab3a1
carlos006gc <carlos.65613@gmail.com>
2024-03-20 14:20:02 -0300

    Icons Adicionados

--- src/app/views/home/header/header.component.ts
+++ src/app/views/home/header/header.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
-import { NgIconComponent } from '@ng-icons/core';
-
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { ionSearch, ionLogoInstagram, ionLogoFacebook, ionLogoLinkedin, ionLogoTiktok, ionCart, ionLogIn } from '@ng-icons/ionicons';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [NgIconComponent],
   template: '<ng-icon [svg]="icon" />',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  viewProviders: [provideIcons({ ionSearch, ionLogoInstagram, ionLogoFacebook, ionLogoLinkedin, ionLogoTiktok, ionCart, ionLogIn })]
+
+
 })
 export class HeaderComponent {
   @Input() icon: string | undefined; // Definindo o input 'icon'
